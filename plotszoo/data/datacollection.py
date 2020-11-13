3ecafe8e5d04e40e00fc00e4d64b235fdbda49fa
Federico Galatolo <galatolo.federico@gmail.com>
2020-11-13 16:06:46 +0100

    wrote dropna function in DataCollection

--- plotszoo/data/datacollection.py
+++ plotszoo/data/datacollection.py
@@ -1,17 +1,8 @@
-
+import pandas as pd
 
 class DataCollection:
     def __init__(self):
         self.data_types = []
-    
-    def is_scalars(self):
-        return "scalars" in self.data_types
-    
-    def is_series(self):
-        return "series" in self.data_types
-    
-    def is_both(self):
-        return self.is_scalars() and self.is_series()
 
     def create_scalar_from_series(self, scalar_name, agg_fn):
         assert self.is_both(), "To create a scalar from series the DataCollection must have both scalars and series"
@@ -21,3 +12,21 @@ class DataCollection:
             newscalars.append(agg_fn(series))
 
         self.scalars[scalar_name] = newscalars
+
+    def dropna(self, columns):
+        assert self.is_scalars(), "You must have scalars to drop"
+        null_indices = self.scalars.index[pd.isnull(self.scalars[columns]).any(1)].tolist()
+        
+        self.scalars = self.scalars.drop(null_indices)
+        if self.is_series():
+            for null_index in null_indices:
+                del self.series[null_index]
+
+    def is_scalars(self):
+        return "scalars" in self.data_types
+    
+    def is_series(self):
+        return "series" in self.data_types
+    
+    def is_both(self):
+        return self.is_scalars() and self.is_series()
