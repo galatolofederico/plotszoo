4609d940887ca2a376f314daf3551d4689978883
Federico Galatolo <galatolo.federico@gmail.com>
2020-11-13 14:24:39 +0100

    Pulling series from wandb

--- plotszoo/data/wandbdata.py
+++ plotszoo/data/wandbdata.py
@@ -27,33 +27,33 @@ class WandbData:
     def pull_scalars(self, state="finished"):
         assert len(self.data_types) == 0, "You can pull the data once for each data object"
 
-        runs = None
-
+        self.runs = None
         if self.cache and os.path.exists(self.cache_file) and not self.force_update:
             print("[!] Using cache file %s" % (self.cache_file, ))
-            runs = json.load(open(self.cache_file, "r"))
+            self.runs = json.load(open(self.cache_file, "r"))
         else:
             print("[!] Pulling data from wandb")
             wandb_runs = api.runs("%s/%s" % (self.username, self.project), json.loads(self.query))
-            runs = []
+            self.runs = []
             for wandb_run in wandb_runs:
-                runs.append(dict(
-                    id=wandb_run.id,
-                    name=wandb_run.name,
-                    config=dict(wandb_run.config),
-                    summary=wandb_run.summary._json_dict
-                ))
+                if state is None or wandb_run.state == state:
+                    runs.append(dict(
+                        id=wandb_run.id,
+                        name=wandb_run.name,
+                        config=dict(wandb_run.config),
+                        summary=wandb_run.summary._json_dict
+                    ))
 
             if self.cache:
                 if not os.path.isdir(self.cache_dir): os.mkdir(self.cache_dir)
                 if not os.path.isfile(self.cache_file) or self.force_update:
                     print("[!] Saving cache file %s" % (self.cache_file, ))
-                    json.dump(runs, open(self.cache_file, "w"))
+                    json.dump(self.runs, open(self.cache_file, "w"))
 
         one_level_runs = []
         internal_keys = ["config", "summary"]
         keep_keys = ["id", "name"]
-        for run in runs:
+        for run in self.runs:
             one_level_run = dict()
             for internal_key in internal_keys:
                 for key, value in run[internal_key].items():
@@ -65,3 +65,22 @@ class WandbData:
         self.data_types.append("scalars")
         self.scalars = pd.DataFrame(one_level_runs)
 
+
+    def pull_series(self):
+        assert len(self.data_types) == 1 and self.data_types[0] == "scalars", "You have to pull_scalars() before pulling the series"
+
+        self.series = []
+        for run in self.runs:
+            cache_file = os.path.join(self.cache_dir, run["id"]+".csv")
+            series_df = None
+            if self.cache and os.path.exists(cache_file) and not self.force_update:
+                print("[!] Using cache file %s" % (cache_file, ))
+                series_df = pd.read_csv(cache_file)
+            else:
+                print("[!] Pulling data from wandb for run_id=%s" % (run["id"], ))
+                wandb_run = api.run("%s/%s/%s" % (self.username, self.project, run["id"]))
+                series_df = wandb_run.history()
+                if self.cache:
+                    print("[!] Writing cache file %s" % (cache_file, ))
+                    series_df.to_csv(cache_file)
+            self.series.append(series_df)
