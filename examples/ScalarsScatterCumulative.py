e9c1f8426783e13ffe3656366c326c4cf998604b
Federico Galatolo <galatolo.federico@gmail.com>
2020-11-15 13:24:43 +0100

    Wrote example and docs for ScalarsScatterCumulative

---
+++ examples/ScalarsScatterCumulative.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import norm
+import os
+import matplotlib.pyplot as plt
+import plotszoo
+
+x = np.linspace(norm.ppf(0.01), norm.ppf(0.99), 20)
+pdf = pd.DataFrame(norm.pdf(x)/norm.pdf(x).sum(), columns=["pdf"])
+
+data = plotszoo.data.DataCollection()
+data.set_scalars(pdf)
+
+fix, ax = plt.subplots()
+
+scatter_cumulative = plotszoo.scalars.ScalarsScatterCumulative(data, x=None, y="pdf", cumulative_fn=np.sum)
+
+scatter_cumulative.plot(ax)
+
+plt.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), "images/ScalarsScatterCumulative.png"))
--- plotszoo/scalars/scattercumulative.py
+++ plotszoo/scalars/scattercumulative.py
@@ -3,6 +3,23 @@ import numpy as np
 from plotszoo.scalars import ScalarsPlot
 
 class ScalarsScatterCumulative(ScalarsPlot):
+    r"""
+    Plot a scatterplot with some running cumulative function (ex. maximum, minimum, mean, sum, ...)
+
+    Args:
+        :data: :class:`plotszoo.data.DataCollection` with some scalars
+        :x: Index to use as x in the scatter plot (set to ``None`` to use the :class:`DataFrame` index)
+        :y: Index to use as y in the scatter plot (required)
+        :cumulative_fn: Cumulative function to use (Default: ``np.max``)
+
+    Example:
+
+    .. literalinclude:: ../../examples/ScalarsScatterCumulative.py
+
+    .. image:: ../../examples/images/ScalarsScatterCumulative.png
+        :width: 600
+    
+    """
     def __init__(self, data, x, y, cumulative_fn=np.max):
         super(ScalarsScatterCumulative, self).__init__(data)
         self.x = x
@@ -10,10 +27,17 @@ class ScalarsScatterCumulative(ScalarsPlot):
         self.cumulative_fn = cumulative_fn
 
     def plot(self, ax, sort=False):
+        r"""
+        Plot the cumulative scatter plot
+
+        Args:
+            :ax: :mod:`matplotlib` ax to plot to
+            :sort: Sort the x data (Default: ``False``)
+        """
         s = []
         for index, row in self.data.scalars.iterrows():
             s.append(dict(
-                x=row[self.x],
+                x=row[self.x] if self.x is not None else index,
                 y=row[self.y]
             ))
         if sort:
