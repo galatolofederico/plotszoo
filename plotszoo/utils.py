225030cba37122e96dd9d26a5e9c2f3632f7e5a2
Federico Galatolo <galatolo.federico@gmail.com>
2020-11-19 13:43:20 +0100

    typo fix

--- plotszoo/utils.py
+++ plotszoo/utils.py
@@ -8,21 +8,22 @@ import os
 
 def savefig(fig, filename, create_dir_tree=True, savefig_kwargs={}):
     r"""
-    Save a :mod:`matplotlib` figure
+    Save a :mod:`matplotlib` figure and creates the directory tree if it do not exists
 
     Args:
         :fig: :mod:`matplotlib` figure to save
         :filename: The figure filename
-        :create_dir_tree: Create the directory tree if it does not exists (Default: ``{}``)
-        :savefig_kwargs: kwargs for ``plt.savefig``
+        :create_dir_tree: Create the directory tree if it does not exists (Default: ``True``)
+        :savefig_kwargs: kwargs for ``plt.savefig`` (Default: ``{}``) 
     
     Example:
         >>> plotszoo.utils.savefig("./plots/nice/first.png")
     
     Creates the folders ``./plots`` and ``./plots/nice`` if they do not exists and then calls ``fig.savefig(filename, **savefig_kwargs)``
     """
-    path = os.path.dirname(filename)
-    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    if create_dir_tree:
+        path = os.path.dirname(filename)
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
     fig.savefig(filename, **savefig_kwargs)
 
 
