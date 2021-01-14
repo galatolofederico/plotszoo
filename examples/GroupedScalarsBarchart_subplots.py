251ca77c43d2f61febf36a38c6791423b2f9ff00
Federico Galatolo <galatolo.federico@gmail.com>
2021-01-14 15:04:01 +0100

    Multiple targets in GroupedScalarsBarchart

--- examples/GroupedScalarsBarchart_subplots.py
+++ examples/GroupedScalarsBarchart_subplots.py
@@ -11,12 +11,12 @@ scalars = pd.DataFrame(data= np.c_[iris["data"], iris["target"]], columns=iris["
 data = plotszoo.data.DataCollection()
 data.set_scalars(scalars)
 
-fig, ax = plt.subplots()
+fig, axes = plt.subplots(2, 2)
 
 yticks_fn = lambda x: iris["target_names"][int(x)]
-
-barchart = plotszoo.scalars.grouped.GroupedScalarsBarchart(data, ["target"], iris["feature_names"])
-barchart.plot(ax, title="Iris", nbins=20, grid=True, yticks_fn=yticks_fn)
+for feature, ax in zip(iris["feature_names"], axes.flatten()):
+    barchart = plotszoo.scalars.grouped.GroupedScalarsBarchart(data, ["target"], feature)
+    barchart.plot(ax, title=feature, nbins=20, grid=True, yticks_fn=yticks_fn)
 
 fig.set_size_inches(20, 10)
-fig.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), "images/GroupedScalarsBarchart.png"))
+fig.savefig(os.path.join(os.path.dirname(os.path.realpath(__file__)), "images/GroupedScalarsBarchart_subplots.png"))
--- plotszoo/scalars/grouped/barchart.py
+++ plotszoo/scalars/grouped/barchart.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 from plotszoo.scalars import ScalarsPlot
 from plotszoo.utils import confidence_interval
@@ -10,23 +11,28 @@ class GroupedScalarsBarchart(ScalarsPlot):
     Args:
         :data: :class:`plotszoo.data.DataCollection` with some scalars
         :groups: A list of indices to group by
-        :target: The index to plot
+        :targets: One or a list of targets to plot
     
     Example:
+
+    .. literalinclude:: ../../examples/GroupedScalarsBarchart_subplots.py
+
+    .. image:: ../../examples/images/GroupedScalarsBarchart_subplots.png
+        :width: 600
 
     .. literalinclude:: ../../examples/GroupedScalarsBarchart.py
 
     .. image:: ../../examples/images/GroupedScalarsBarchart.png
         :width: 600
     """
-    def __init__(self, data, groups, target):
+    def __init__(self, data, groups, targets):
         super(GroupedScalarsBarchart, self).__init__(data)
         self.groups = groups
-        self.target = target
+        self.targets = targets if isinstance(targets, list) else [targets]
 
-        self.grouped_scalars = data.scalars.groupby(groups).agg({target: [np.mean, confidence_interval]}) 
+        self.grouped_scalars = data.scalars.groupby(groups).agg({target: [np.mean, confidence_interval] for target in self.targets}) 
 
-    def plot(self, ax, title=None, nbins=10, grid=False, yticks_fn=None):
+    def plot(self, ax, title=None, nbins=10, grid=False, yticks_fn=None, legend=None):
         r"""
         Plot the grouped mean barchart
 
@@ -36,16 +42,28 @@ class GroupedScalarsBarchart(ScalarsPlot):
             :nbins: Number of ticks in the x axis (Default: 10)
             :grid: Show vertical grid lines (Default: ``False``)
             :yticks_fn: A function to convert each index to text (Default: ``None``)
+            :legend: Plot the legend (Default: auto)
         
         Returns:
             Grouped scalars :mod:`pandas` DataFrame
         
         """
-        self.grouped_scalars[self.target]["mean"].plot.barh(ax=ax, xerr=self.grouped_scalars[self.target]["confidence_interval"], capsize=4, xlabel="")
+
+        if legend is None: legend = len(self.targets) > 1
+
+        means = self.grouped_scalars.loc[:, (slice(None), "mean")]
+        means.columns = means.columns.droplevel(1)
+        
+        errors = self.grouped_scalars.loc[:, (slice(None), "confidence_interval")]
+        errors.columns = errors.columns.droplevel(1)
+        
+        means.plot.barh(ax=ax, xerr=errors, capsize=4, xlabel="", legend=legend)
+
         if title is not None: ax.title.set_text(title)
         if yticks_fn is not None:
             xticks = [yticks_fn(index) for index, _ in self.grouped_scalars.iterrows()]
             ax.set_yticklabels(xticks)
+        
         ax.locator_params(nbins=nbins, axis="x")
         if grid: ax.grid(which="both", axis="x", linestyle="--")
 
